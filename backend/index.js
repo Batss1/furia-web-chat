072069f9b903226cf5f43a9338bdc9e9e1eaef41
Gabriel Batista <gabriel.rodrigues250306@gmail.com>
2025-04-26 16:57:27 -0300

    feat: configura Socket.IO

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,35 @@
 const express = require('express');
+const cors = require('cors');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+app.use(cors());
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('Novo usuário conectado:', socket.id);
+
+    socket.on('message', (message) => {
+        if (message.text.startsWith('/')) {
+            io.emit('message', {
+                text: 'Comando recebido: ${message.text}', 
+                isBot: true,
+            });
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectado:', socket.id);
+    });
+});
+
+httpServer.listen(3001, () => {
+    console.log('Backend Socket.io rodando na porta 3001');
+});
