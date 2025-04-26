b35a62d7e76629e5e0ba2b7c2ad74ef2086666da
Gabriel Batista <gabriel.rodrigues250306@gmail.com>
2025-04-26 15:06:53 -0300

    feat: Cria Hook de Conexão

---
+++ src/hooks/useSocket.ts
@@ -0,0 +1,15 @@
+import { io, Socket } from 'socket.io-client';
+import { useEffect, useState } from 'react';
+
+
+export default function useSocket() {
+    const [socket, setSocket] = useState<Socket | null>(null);
+
+    useEffect(() => {
+        const newSocket = io(url);
+        setSocket(newSocket);
+        return () => { newSocket.disconnect(); };
+    }, [url]);
+
+    return socket;
+} 
