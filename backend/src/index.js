ffc63eb5bd33467e63603fd0f99a4594c1008231
Gabriel Batista <gabriel.rodrigues250306@gmail.com>
2025-05-01 15:22:03 -0300

    feat: cria messageRoutes e configura getMessages, sendMessages e getUsersForSidebar

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/database.js";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 
 app.listen(PORT, () => {
