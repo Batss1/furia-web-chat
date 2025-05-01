72606b1b0829b520ad0f001acb2cf7437ea71311
Gabriel Batista <gabriel.rodrigues250306@gmail.com>
2025-05-01 17:19:16 -0300

    feat: cria paginas principais

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,36 @@
-import ChatWindow from "./components/ChatWindow"
 
-function App() {
-  
+import Navbar from "./components/Navbar"
+
+import HomePage from "./pages/HomePage"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
+import SettingsPage from "./pages/SettingsPage"
+import ProfilePage from "./pages/ProfilePage"
+
+import React from 'react'
+
+import { Routes, Route } from 'react-router-dom'
+import { useAuthStore } from "./store/authStore"
+
+
+
+const App = () => {
+  const { authUser } = useAuthStore() ;
   return (
-    <div className="h-screen ">
-      <h1 className="font-furia">Teste</h1>
-      <ChatWindow />
+    <div>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />}/> 
+        <Route path="/signup" element={<SignUpPage />}/> 
+        <Route path="/login" element={<LoginPage />}/> 
+        <Route path="/settings" element={<SettingsPage />}/> 
+        <Route path="/profile" element={<ProfilePage />}/> 
+      </Routes>
+
     </div>
   );
-}
+};
 
 export default App
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,13 @@
+import daysyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
 export default {
     content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",,
     ],
     theme: {
-      extend: {
-        color: {
-          'furia': '#FF6F61',
-          'furia-light': '#FFB3A0',
-          'furia-dark': '#C94C3D',
-          'furia-darkest': '#7A2A2A',
-        },
-        fontFamily: {
-          'furia': ['Furia', 'sans-serif'],
-      },
+      extend: {},
     },
-    plugins: [],
-  }
-}
+    plugins: [daysyui],
+};
